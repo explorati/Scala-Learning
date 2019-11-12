c817d175aca2a3530627bf36911cd155dd9a0af3
explorati <w1748419447@163.com>
2019-11-12 19:16:57 +0800

    HelloWorld测试

---
+++ scala-learning/src/main/scala/com/explorati/scala/HelloWorld.scala
@@ -0,0 +1,14 @@
+package com.explorati.scala
+
+/**
+  *
+  * @ Author : Weijian_Wang
+  * @ Date : Created in 19:00 2019/11/12 0012
+  * @ Description ： 
+  */
+object HelloWorld {
+
+  def main(args : Array[String]) : Unit = {
+    println("Hello World... FROM IDEA...")
+  }
+}
